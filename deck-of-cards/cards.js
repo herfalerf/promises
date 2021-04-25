7bfc7f32f30f1a3c34b956c27027bee29e178f61
herfalerf <jpendy@gmail.com>
2021-04-24 21:42:19 -0500

    finished card problem 3

--- deck-of-cards/cards.js
+++ deck-of-cards/cards.js
@@ -13,23 +13,56 @@
 
 //2
 
-axios
-  .get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-  .then((deck) => {
-    return axios.get(
-      `https://deckofcardsapi.com/api/deck/${deck.data.deck_id}/draw/?count=1`
-    );
-  })
-  .then((card) => {
-    console.log("this is card 1");
-    console.log(card.data.cards[0].value);
-    console.log(card.data.cards[0].suit);
-    return axios.get(
-      `https://deckofcardsapi.com/api/deck/${card.data.deck_id}/draw/?count=1`
-    );
-  })
-  .then((card) => {
-    console.log("this is card 2");
-    console.log(card.data.cards[0].value);
-    console.log(card.data.cards[0].suit);
-  });
+// axios
+//   .get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+//   .then((deck) => {
+//     return axios.get(
+//       `https://deckofcardsapi.com/api/deck/${deck.data.deck_id}/draw/?count=1`
+//     );
+//   })
+//   .then((card) => {
+//     console.log("this is card 1");
+//     console.log(card.data.cards[0].value);
+//     console.log(card.data.cards[0].suit);
+//     return axios.get(
+//       `https://deckofcardsapi.com/api/deck/${card.data.deck_id}/draw/?count=1`
+//     );
+//   })
+//   .then((card) => {
+//     console.log("this is card 2");
+//     console.log(card.data.cards[0].value);
+//     console.log(card.data.cards[0].suit);
+//   });
+
+//3
+let deckId;
+const $cardBtn = $("#get_card");
+const $cardPile = $("#card_pile");
+
+$(function () {
+  axios
+    .get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+    .then((deck) => {
+      console.log(deck.data.deck_id);
+      return (deckId = deck.data.deck_id);
+    });
+});
+
+function getCard() {
+  axios
+    .get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+    .then((card) => {
+      console.log(card.data.cards[0].value);
+      console.log(card.data.cards[0].suit);
+      console.log(card.data.remaining);
+      $cardPile.append(`<div class='card'>
+                            <div>${card.data.cards[0].value}</div>
+                            <div>${card.data.cards[0].suit}</div>
+                        </div>`);
+    })
+    .catch((err) => {
+      console.log(`Oops, there was a problem :( ${err}`);
+    });
+}
+
+$cardBtn.on("click", getCard);
